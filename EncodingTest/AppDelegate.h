aca943a1b7619d5ae670407c77edbd546ce80ada
Gregory Casamento <greg.casamento@gmail.com>
2025-12-05 17:32:58 -0500

    Initial Commit

---
+++ EncodingTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  EncodingTest
+//
+//  Created by Gregory Casamento on 12/5/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ EncodingTest/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  EncodingTest
+//
+//  Created by Gregory Casamento on 12/5/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
